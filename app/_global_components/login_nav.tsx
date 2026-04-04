23035c41e4db4098d9e36e2daf28ed07823118ed
ROYY-Y <punkung159951@gmail.com>
2026-04-04 12:44:06 +0900

    Pun init login-navbar

---
+++ app/_global_components/login_nav.tsx
@@ -0,0 +1,9 @@
+import style  from "./login_nav.module.css"
+
+export default function Navbar(){
+    return (<>
+        <nav className={style.logNav}>
+            <img src={""}></img>
+        </nav>
+    </>)   
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+import Navbar from "../_global_components/login_nav"
+
+export default function Login(){
+    return(<>
+        <Navbar></Navbar>
+    </>)
+}

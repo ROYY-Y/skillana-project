a79565f03ed77c8be1106b6ae10cbe9601218514
ROYY-Y <punkung159951@gmail.com>
2026-04-13 14:15:32 +0900

    Pun update create-account interact

--- app/create-account/_components/create_form.tsx
+++ app/create-account/_components/create_form.tsx
@@ -4,7 +4,8 @@ import Link from "next/link"
 import style from "./create_form.module.css"
 import InputComponent from "@/app/_global_components/authen_pages/login_input"
 
-import { useRef } from "react"
+import { useRef, useState } from "react"
+import { error } from "console"
 
 export default function CreateAccountForm(){
     const firstNameRef = useRef<HTMLInputElement>(null)
@@ -13,6 +14,16 @@ export default function CreateAccountForm(){
     const passRef = useRef<HTMLInputElement>(null)
     const confirmPassRef = useRef<HTMLInputElement>(null)
 
+    const [errors, setErrors] = useState<{
+        [key: string]: [boolean, string]
+    }>({
+        firstName: [false, ""],
+        lastName: [false, ""],
+        email: [false, ""],
+        password: [false, ""],
+        confirmPassword: [false, ""]
+    });
+
     function handleSubmit(){
         const firstName = firstNameRef.current?.value;
         const lastName = lastNameRef.current?.value;
@@ -20,7 +31,20 @@ export default function CreateAccountForm(){
         const pass = passRef.current?.value;
         const confirmPass = confirmPassRef.current?.value;
 
-        console.log({firstName, lastName, email, pass, confirmPass})
+        const arr = {
+            firstName : firstName,
+            lastName : lastName,
+            email : email,
+            password : pass,
+            confirmPassword : confirmPass
+        }
+        
+        Object.entries(arr).forEach(([key,value])=>{
+            if(!value) {
+                setErrors(prev => ({...prev, [key]: [true, `Please enter ${key == "email" ? "an" : "a"} ${key == "confirmPassword" ? "confirm password": key.toLowerCase()}.`]}));
+            }
+        })
+        
 
     }
 
@@ -32,13 +56,23 @@ export default function CreateAccountForm(){
             </div>
             
             <div style={{display: "flex", gap : "1em"}}>
-                <InputComponent ref = {firstNameRef} label="First Name" size="small" placeholder="Enter your first name"></InputComponent>
-                <InputComponent ref = {lastNameRef} label="Last Name" size="small" placeholder="Enter your last name"></InputComponent>
+                <InputComponent ref = {firstNameRef} label="First Name" size="small" placeholder="Enter your first name" 
+                isError={errors.firstName[0]} message={errors.firstName[1]}
+                />
+                <InputComponent ref = {lastNameRef} label="Last Name" size="small" placeholder="Enter your last name"
+                isError={errors.lastName[0]} message={errors.lastName[1]}
+                />
             </div>
 
-            <InputComponent ref = {emailRef} label="Email" size="large" placeholder="Enter your email" isPassword = {false}></InputComponent>
-            <InputComponent ref = {passRef} label="Password" size="large" placeholder="Enter your password" isPassword = {true}></InputComponent>
-            <InputComponent ref = {confirmPassRef} label="Confirm Password" size="large" placeholder="Confirm your password" isPassword = {true}></InputComponent>
+            <InputComponent ref = {emailRef} label="Email" size="large" placeholder="Enter your email" isPassword = {false}
+            isError={errors.email[0]} message={errors.email[1]}
+            />
+            <InputComponent ref = {passRef} label="Password" size="large" placeholder="Enter your password" isPassword = {true}
+            isError={errors.password[0]} message={errors.password[1]}
+            />
+            <InputComponent ref = {confirmPassRef} label="Confirm Password" size="large" placeholder="Confirm your password" isPassword = {true}
+            isError={errors.confirmPassword[0]} message={errors.confirmPassword[1]}
+            />
             <div style={{display : "flex", flexDirection : "column", gap : '.5em', alignItems : "center"}}>
                 <button className= {style.createBtn} onClick={handleSubmit}>Sign up</button>
                 <p>Already have an account? <Link href= "/login" className= {style.login}>Login</Link> </p>

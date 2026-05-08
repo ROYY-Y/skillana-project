7c72eecc46e7ea6ae38c8d9abdeffeb99d12342d
ROYY-Y <punkung159951@gmail.com>
2026-05-08 21:27:26 +0900

    Pun commit resume

--- app/resume-export/[id]/components/templates/template2.tsx
+++ app/resume-export/[id]/components/templates/template2.tsx
@@ -1,11 +1,31 @@
 "use client"
 import {ResumeData} from "./type/resume"
+import mainstyles from "./main_style.module.css"
+import styles from "./template2.module.css"
+import { useEffect,useState } from "react"
 type Props = {
   data: ResumeData;
   size: "full" | "small";
 };
-export default function Template2({ data, size }: Props){
-    return (<>
-        
-    </>)
+
+export default function Template4({ data, size }: Props) {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    console.log(data)
+  const containerClass = isMounted 
+    ? (size === "full" ? mainstyles.ResumeFull : mainstyles.ResumeSmall)
+    : mainstyles.Resume; 
+
+  if (!data) {
+    return <div>Loading...</div>; 
+  }
+  return (
+    <>
+        <div className ={size == "full" ? mainstyles.ResumeFull :mainstyles.ResumeSmall } >
+            
+        </div>
+    </>
+  );
 }
--- app/resume-export/[id]/components/templates/template5.tsx
+++ app/resume-export/[id]/components/templates/template5.tsx
@@ -1,11 +1,31 @@
 "use client"
 import {ResumeData} from "./type/resume"
+import mainstyles from "./main_style.module.css"
+import styles from "./template5.module.css"
+import { useEffect,useState } from "react"
 type Props = {
   data: ResumeData;
   size: "full" | "small";
 };
-export default function Template5({ data, size }: Props){
-    return (<>
-        
-    </>)
+
+export default function Template4({ data, size }: Props) {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    console.log(data)
+  const containerClass = isMounted 
+    ? (size === "full" ? mainstyles.ResumeFull : mainstyles.ResumeSmall)
+    : mainstyles.Resume; 
+
+  if (!data) {
+    return <div>Loading...</div>; 
+  }
+  return (
+    <>
+        <div className ={size == "full" ? mainstyles.ResumeFull :mainstyles.ResumeSmall } >
+          
+        </div>
+    </>
+  );
 }

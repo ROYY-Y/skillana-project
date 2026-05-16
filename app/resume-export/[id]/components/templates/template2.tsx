b0f72acd976c681601f9bef5f0f12821eb52cc01
namelessAOE <Bahneendaral@gmail.com>
2026-05-16 13:38:43 +0700

    Run make template2

--- app/resume-export/[id]/components/templates/template2.tsx
+++ app/resume-export/[id]/components/templates/template2.tsx
@@ -24,7 +24,42 @@ export default function Template4({ data, size }: Props) {
   return (
     <>
         <div className ={size == "full" ? mainstyles.ResumeFull :mainstyles.ResumeSmall } >
-            
+            <div className={styles.name}>{data.firstName} {data.lastName}</div>
+            <div className={styles.profile}>
+             <img src={data.profileImg} alt="Image profile" className={styles.profileImg} />
+             <div className={styles.contact}><h3>Contact</h3></div>
+              <div className={styles.contactInfo}>
+                <ul className={styles.contactList}>
+                  <li><img src={"/icon/phone.png"} alt="Phone" /> {data.contact.phoneNumber}</li>
+                  <li><img src={"/icon/mail.png"} alt="Email" /> {data.email}</li>
+                  <li><img src={"/icon/location.png"} alt="Address" /> {data.contact.address}</li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.aboutMeContainer}>
+              <div className={styles.header}>
+                <img src={"/icon/user.png"} alt="User" className={styles.userIcon} />
+                <h3>About Me</h3>
+              </div>
+              <p style={{ margin: "1rem 0 0 4.5rem" }}>{data.aboutMe}</p>
+            </div>
+            <section className={styles.resumeSection}>
+  <h2 className={styles.sectionTitle}>◯ EDUCATION</h2>
+
+  <div className={styles.entry}>
+    <p className={styles.entryDate}>(2011–2015)</p>
+    <h4 className={styles.entryCompany}>WARDIERE UNIVERSITY</h4>
+    <p className={styles.entryRole}>Bachelor of Marketing</p>
+    <p className={styles.entryGpa}>3.65</p>
+  </div>
+
+  <div className={styles.entry}>
+    <p className={styles.entryDate}>(2014–2018)</p>
+    <h4 className={styles.entryCompany}>WARDIERE UNIVERSITY</h4>
+    <p className={styles.entryRole}>BA Sales and Commerce</p>
+    <p className={styles.entryGpa}>3.74</p>
+  </div>
+</section>
         </div>
     </>
   );
